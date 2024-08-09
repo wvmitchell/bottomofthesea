9d1f032187b3a4a48f7c9bdab60e293b6f067be6
Will Mitchell <wvmitchell@gmail.com>
2024-08-09 16:02:08 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+http.createServer(function(req, res) {
+	res.write('On the way to real fullstack!')
+	res.end()
+}).listen(3000)
+
+console.log('Server started on port 3000')
