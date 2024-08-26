7e3f15bd56ddc3e7ddab201dc96a3b3820b96779
Will Mitchell <wvmitchell@gmail.com>
2024-08-26 13:21:20 -0400

    created websocket server

---
+++ indexws.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const server = require('http').createServer()
+const app = express()
+
+app.get('/', (_, res) => {
+	res.sendFile('index.html', { root: __dirname })
+})
+
+server.on('request', app)
+server.listen(3001, () => {
+	console.log('Server started on port 3001')
+})
+
+/** Begin WebSockets **/
+const WebSocketServer = require('ws').Server
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size
+	console.log('Clients connected: ' + numClients)
+
+	wss.broadcast('Current visitors: ' + numClients)
+
+	if(ws.readyState === ws.OPEN) {
+		ws.send('Welcom to the server!')
+	}
+
+	ws.on('close', function close() {
+		console.log('Client disconnected')
+	})
+})
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach(function each(client) {
+		client.send(data)
+	})
+}
