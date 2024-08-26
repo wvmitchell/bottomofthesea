d3194023ff0e8f674503f177310d3250f1e1f2b4
Will Mitchell <wvmitchell@gmail.com>
2024-08-26 13:26:30 -0400

    use correct port

--- indexws.js
+++ indexws.js
@@ -7,8 +7,8 @@ app.get('/', (_, res) => {
 })
 
 server.on('request', app)
-server.listen(3001, () => {
-	console.log('Server started on port 3001')
+server.listen(3000, () => {
+	console.log('Server started on port 3000')
 })
 
 /** Begin WebSockets **/
